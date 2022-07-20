0188781210095366b60ca9cd0874939b5cb06e95
geisondossantos <geison_bf@hotmail.com>
2022-07-20 14:30:43 -0300

    Add pomodo-timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { PomodoroTimer } from './components/pomodoro-timer';
 
 function App() {
-  return <div className="App">Olá mundo!</div>;
+  return (
+    <div className="App">
+      <PomodoroTimer defaultPomodoroTime={1500}></PomodoroTimer>
+    </div>
+  );
 }
 
 export default App;
