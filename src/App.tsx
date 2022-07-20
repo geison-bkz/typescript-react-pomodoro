962028ced3b5c1ddb47250f99f42989eb6409c02
geisondossantos <geison_bf@hotmail.com>
2022-07-20 14:43:43 -0300

    Add second-to-time

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { PomodoroTimer } from './components/pomodoro-timer';
 function App() {
   return (
     <div className="App">
-      <PomodoroTimer defaultPomodoroTime={1500}></PomodoroTimer>
+      <PomodoroTimer defaultPomodoroTime={3660}></PomodoroTimer>
     </div>
   );
 }
